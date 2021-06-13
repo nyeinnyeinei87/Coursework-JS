36e9a66ad4f0c272be6e1b0054633961a332fc83
nyeinnyeinei <nyeinnyeinei1987@gmail.com>
2021-06-13 14:19:58 +0800

    adding 2 files together

---
+++ switch.js
@@ -0,0 +1,16 @@
+let country = prompt("Enter a country:singapore or other country");
+switch (country) {
+    case 'singapore':
+      alert( "We've got airline for this singapore country!" );
+      break;
+  
+    case 'japan':
+    case 'USA':
+    case 'korea':
+    case 'malaysia':
+      alert( 'Welcome,we support airline these country too' );
+      break;
+  
+    default:
+      alert( 'We support only for singapore,japan,USA,korea & Malaysia,We hope that this page looks ok!' );
+  }
